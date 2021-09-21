a8fa07d046cc8fe0e4a35745e7bdd7dcdc90e5e2
Jim Coughlan <53486481+JimCou@users.noreply.github.com>
2021-09-21 14:38:55 +0100

    Add files via upload

---
+++ PWModal.js
@@ -0,0 +1,95 @@
+$(function(){
+    var copyContent = $("#PWpic1").clone(); 
+var copyContent1 = $("#PWdes1").clone();
+    $('.PWpic1').append(copyContent); 
+	$('.PWdes1').append(copyContent1); 
+
+}); 
+
+$(function(){
+    var copyContent = $("#PWpic2").clone(); 
+var copyContent1 = $("#PWdes2").clone();
+    $('.PWpic2').append(copyContent); 
+	$('.PWdes2').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic3").clone(); 
+var copyContent1 = $("#PWdes3").clone();
+    $('.PWpic3').append(copyContent); 
+	$('.PWdes3').append(copyContent1); 
+
+});
+$(function(){
+    var copyContent = $("#PWpic4").clone(); 
+var copyContent1 = $("#PWdes4").clone();
+    $('.PWpic4').append(copyContent); 
+	$('.PWdes4').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic5").clone(); 
+var copyContent1 = $("#PWdes5").clone();
+    $('.PWpic5').append(copyContent); 
+	$('.PWdes5').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic6").clone(); 
+var copyContent1 = $("#PWdes6").clone();
+    $('.PWpic6').append(copyContent); 
+	$('.PWdes6').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic7").clone(); 
+var copyContent1 = $("#PWdes7").clone();
+    $('.PWpic7').append(copyContent); 
+	$('.PWdes7').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic8").clone(); 
+var copyContent1 = $("#PWdes8").clone();
+    $('.PWpic8').append(copyContent); 
+	$('.PWdes8').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic9").clone(); 
+var copyContent1 = $("#PWdes9").clone();
+    $('.PWpic9').append(copyContent); 
+	$('.PWdes9').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic10").clone(); 
+var copyContent1 = $("#PWdes10").clone();
+    $('.PWpic10').append(copyContent); 
+	$('.PWdes10').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic11").clone(); 
+var copyContent1 = $("#PWdes11").clone();
+    $('.PWpic11').append(copyContent); 
+	$('.PWdes11').append(copyContent1); 
+
+});
+
+$(function(){
+    var copyContent = $("#PWpic12").clone(); 
+var copyContent1 = $("#PWdes12").clone();
+    $('.PWpic12').append(copyContent); 
+	$('.PWdes12').append(copyContent1); 
+
+});
+
